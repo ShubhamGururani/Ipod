ea9af0a21188e3f5a9a5a3b8423d79b1993ff154
ShubhamGururani <shubhamgururani@gmail.com>
2021-05-10 18:22:12 +0530

    created screen and menu layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Keypad from './Keypad';
-
+import Screen from './Screen';
 
 class App extends React.Component {
   
   render(){
     return(
       <div className="App">
+        <Screen/>
         <Keypad/>
       </div>
     );
--- src/Screen.js
+++ src/Screen.js
@@ -4,8 +4,24 @@ class Screen extends React.Component{
     render(){
         return(
             <div className="screen-wrapper">
-
+                <div className="battery">
+                    <img 
+                        src="https://image.flaticon.com/icons/png/128/3103/3103460.png" 
+                        alt="battery"
+                    />
+                </div>
+                <div className="side-menu">
+                    <h1>iPod.js</h1>
+                    <ul>
+                        <li>Songs</li>
+                        <li className="active">Albums</li>
+                        <li>Artists</li>
+                        <li>Playlists</li>
+                    </ul>
+                </div>
             </div>
         );
     }
 }
+
+export default Screen;
